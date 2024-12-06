503d778a1b06054b081f488ab5af28bba7cff91d
Yuichi Goto <949545+yasaichi@users.noreply.github.com>
2024-12-06 13:50:10 +0900

    Extract lua script

---
+++ envoy/filter.lua
@@ -0,0 +1,17 @@
+function envoy_on_request(request_handle)
+  local meta = request_handle:streamInfo():dynamicMetadata():get("envoy.filters.http.jwt_authn")
+
+  if (meta["jwt_payload"] == nil) then
+    request_handle:logErr("No JWT payload found. Set `payload_in_metadata` option.")
+    request_handle:respond({ [":status"] = "500", }, "Something went wrong.")
+    return
+  end
+
+  if not IsAuthorized(meta["jwt_payload"], "scope", "user.profile") then
+    request_handle:respond({ [":status"] = "403", }, "JWT validation failed.")
+  end
+end
+
+function IsAuthorized(payload, claim, value)
+  return type(payload[claim]) == "string" and string.find(payload[claim], value) ~= nil
+end
