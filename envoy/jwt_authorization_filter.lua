3c144275d5cf85127ddb02bac8ab2057496d5165
Yuichi Goto <949545+yasaichi@users.noreply.github.com>
2024-12-11 17:50:23 +0900

    Rename the filter file

--- envoy/jwt_authorization_filter.lua
+++ envoy/jwt_authorization_filter.lua
@@ -1,3 +1,7 @@
+local function isAuthorized(payload, claim, value)
+  return type(payload[claim]) == "string" and string.find(payload[claim], value) ~= nil
+end
+
 function envoy_on_request(request_handle)
   local meta = request_handle:streamInfo():dynamicMetadata():get("envoy.filters.http.jwt_authn")
 
@@ -7,11 +11,7 @@ function envoy_on_request(request_handle)
     return
   end
 
-  if not IsAuthorized(meta["jwt_payload"], "scope", "user.profile") then
+  if not isAuthorized(meta["jwt_payload"], "scope", "user.profile") then
     request_handle:respond({ [":status"] = "403", }, "JWT validation failed.")
   end
-end
-
-function IsAuthorized(payload, claim, value)
-  return type(payload[claim]) == "string" and string.find(payload[claim], value) ~= nil
 end
